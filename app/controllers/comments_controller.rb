27a30019bf8ab516528f9c29ebd5a98f70026ef5
Gabriel Saraiva <extremez3r0@gmail.com>
2017-08-06 20:17:26 -0300

    Basic Authentication Methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 
+	http_basic_authenticate_with name: "biel", password:"gatinho", only: :destroy
+
 	def create
 
 		@article = Article.find(params[:article_id])
